a8d189f3e9e0265e7bca1950cc9647bbce24eddb
nanannannana <rhaehfdl032037@gmail.com>
2023-02-03 01:35:44 +0900

    feat: foreignkey 설정 commit

--- backend/model/index.js
+++ backend/model/index.js
@@ -16,4 +16,21 @@ db.Sequelize = Sequelize;
 db.Window = require('./window')(sequelize, Sequelize);
 db.User = require('./user')(sequelize, Sequelize);
 
+// foreign key 설정
+// 1. User <=> Window
+// 1-1. User(부모키) 설정
+db.User.hasMany(db.Window, {
+  foreignKey: 'user_id',
+  sourceKey: 'user_id',
+  onDelete: 'casecade',
+  onUpdate: 'casecade',
+});
+// 1-2. Window(자식키) 설정
+db.Window.belongsTo(db.User, {
+  foreignKey: 'user_id',
+  sourceKey: 'user_id',
+  onDelete: 'casecade',
+  onUpdate: 'casecade',
+});
+
 module.exports = db;
